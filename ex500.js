3f57457e024ff00f6bfa2859dc03ad90b7857e71
santandanao <131091831+santandanao@users.noreply.github.com>
2024-01-14 17:57:25 +0900

    Update ex500.js

--- ex500.js
+++ ex500.js
@@ -1,1 +1,8 @@
-
+function gazo101(){
+  document.getElementById('who101').innerHTML=
+    '<img src="./image/DJL2qVDUEAAOOkO.jpg" alt="昔の金の国">'
+  ;}
+function gazo102(){
+  document.getElementById('who101').innerHTML=
+    '<img src="./image/617d81d75b649fcb5976fb5385c55584df9df193-thumb-630xauto-123925.jpg" alt="今の金の国">'
+  ;}
