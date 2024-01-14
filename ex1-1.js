d367dce4ec8beb4165250f17a8c7fcb65dc7c393
santandanao <131091831+santandanao@users.noreply.github.com>
2024-01-14 17:55:17 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -4,6 +4,5 @@ function gazo101(){
   ;}
 function gazo102(){
   document.getElementById('who101').innerHTML=
-    '<img src="./image/617d81d75b649fcb5976fb5385c55584df9df193-thumb-630xauto-123925.jpg
-" alt="今の金の国">'
+    '<img src="./image/617d81d75b649fcb5976fb5385c55584df9df193-thumb-630xauto-123925.jpg" alt="今の金の国">'
   ;}
