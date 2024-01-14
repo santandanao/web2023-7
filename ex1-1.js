57ad23456a0cf2e0fec43109dfb063b4557a2ea3
santandanao <131091831+santandanao@users.noreply.github.com>
2024-01-14 18:00:10 +0900

    Delete ex1-1.js

--- ex1-1.js
+++
@@ -1,8 +0,0 @@
-function gazo101(){
-  document.getElementById('who101').innerHTML=
-    '<img src="./image/DJL2qVDUEAAOOkO.jpg" alt="昔の金の国" width="450" height="300">'
-  ;}
-function gazo102(){
-  document.getElementById('who101').innerHTML=
-    '<img src="./image/617d81d75b649fcb5976fb5385c55584df9df193-thumb-630xauto-123925.jpg" alt="今の金の国" width="450" height="300">'
-  ;}
