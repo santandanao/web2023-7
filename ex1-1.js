099d1fb5dd3ad128272098ea233f8f7dd775cc4b
santandanao <131091831+santandanao@users.noreply.github.com>
2024-01-14 17:37:47 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,1 +1,8 @@
-
+function gazo101(){
+  document.getElementById('who101').innerHTML=
+    '<img src="" alt="マグロ寿司">'
+  ;}
+function gazo102(){
+  document.getElementById('who101').innerHTML=
+    '<img src="" alt="マグロ">'
+  ;}
